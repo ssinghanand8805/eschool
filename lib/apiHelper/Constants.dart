fbbefabd8bb292fb3f59ac1b80b3e9e63cd7ccd0
sandeep23097 <sandeepsinghmbbs@gmail.com>
2024-04-15 09:55:51 +0530

    update by sandeep profile page

--- lib/apiHelper/Constants.dart
+++ lib/apiHelper/Constants.dart
@@ -2,7 +2,7 @@ class Constants {
   static const String imagesUrl = 'http://172.16.19.96/school3/';
   static const String imagesUrl2 = 'http://172.16.19.96/school3';
   static const String getstudentsubjectUrl = 'webservice/getstudentsubject';
-  static const String authUrl = '"auth/login"';
+  static const String authUrl = 'auth/login';
   static const String getHomeworkUrl = 'webservice/getHomework';
   static const String getClassTimeTableUrl = 'webservice/class_schedule';
   static const String getNotificationsUrl = 'webservice/getNotifications';
