6a698554fdd7e685b1bcf10992b098e5f9ed8524
devAtCriteriontech <developer@criteriontech.in>
2024-06-14 09:45:54 +0530

    work by faheem (communicate completed)

---
+++ lib/presentation/Communicate/Email Sms Log/email_sms_log_controller.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class EmailSmsLogController extends GetxController{
+
+  Rx<TextEditingController> searchC = TextEditingController().obs;
+
+  List<Map<String, dynamic>> data = [
+    {
+      'studentId': 18001,
+      'class': 'Class 4',
+      'section': 'A',
+      'subjectGroup': 'Class 1st Subject Group',
+      'subject': 'Hindi (230)',
+      'homeworkDate': DateTime(2024, 4, 5),
+      'submissionDate': DateTime(2024, 4, 9),
+      'evaluationDate': DateTime(2024, 4, 9),
+      'createdBy': 'Joe Black',
+      'approvedId': 9000,
+    },
+    {
+      'studentId': 18002,
+      'class': 'Class 4',
+      'section': 'A',
+      'subjectGroup': 'Class 1st Subject Group',
+      'subject': 'Hindi (230)',
+      'homeworkDate': DateTime(2024, 4, 5),
+      'submissionDate': DateTime(2024, 4, 9),
+      'evaluationDate': DateTime(2024, 4, 9),
+      'createdBy': 'Kirti Singh',
+      'approvedId': 9000,
+    },
+    // Add more data as needed
+  ];
+
+}
