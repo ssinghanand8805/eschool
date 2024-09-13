f713741ac092c583f97cf1b33ccdcea77b9a30b8
ssinghanand8805 <ssignhanand8805@gmil.com>
2024-09-13 09:51:09 +0530

    change by anand gallery

--- lib/presentation/gallery/controller/gallery_controller.dart
+++ lib/presentation/gallery/controller/gallery_controller.dart
@@ -7,32 +7,28 @@ import '../model/gallery.dart';
 class GalleryController extends GetxController {
   UserData userData = Get.put(UserData());
   ApiRespository apiRespository = ApiRespository(apiClient: Get.find());
-  RxList<GalleryModal> GalleryModalObj = <GalleryModal>[].obs;
+  RxList<GalleryModal> galleryModalObj = <GalleryModal>[].obs;
   late Future<void> fetchDataFuture;
   @override
   void onInit() {
     super.onInit();
     getData();
-
   }
 
   Future<void> getData() async {
+    Map<String, dynamic> body = {};
 
-    Map<String, dynamic> body = {
-    };
-
-    var data = await apiRespository.postApiCallByJson(Constants.getGalleryList, body);
+    var data =
+        await apiRespository.postApiCallByJson(Constants.getGalleryList, body);
     print("DATA @@@@ ${data.body}");
 
     List<GalleryModal> d = [];
-    for(var i =0;i <data.body['listResult'].length; i++)
-    {
-      print("nmjvkjjgkjnnj");
+    for (var i = 0; i < data.body['listResult'].length; i++) {
       d.add(GalleryModal.fromJson(data.body['listResult'][i]));
-      print("nhbhbhhhbbhhb");
+
     }
-    GalleryModalObj.value = d;
-    print("fvffhghnhnhnhnhnhn$d");
+    galleryModalObj.value = d;
+
     update();
   }
 }
--- lib/presentation/gallery/gallery_view.dart
+++ lib/presentation/gallery/gallery_view.dart
@@ -1,33 +1,74 @@
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:lerno/core/app_export.dart';
 
-import '../common_widgets/MainBody.dart';
 import 'controller/gallery_controller.dart';
+import 'model/gallery.dart';
 
 class GalleryPage extends StatefulWidget {
   @override
-  State<GalleryPage> createState() => _GalleryPageState();
+  _GalleryPageState createState() => _GalleryPageState();
 }
 
 class _GalleryPageState extends State<GalleryPage> {
   GalleryController controller = Get.put(GalleryController());
+
   @override
   Widget build(BuildContext context) {
-    return MainBody(
-      label: 'Your Saved\nImages are Here!',
-      imageUrl: 'assets/projectImages/librarypage.jpg',
-      AppbarTitle: 'Images',
-      widget: _buildChildWidget(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Gallery"),
+        backgroundColor: Colors.green.shade200,
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.0,
+          crossAxisSpacing: 4.0,
+          mainAxisSpacing: 4.0,
+        ),
+        itemCount: controller.galleryModalObj.length,
+        itemBuilder: (context, index) {
+          return GridItemWidget(item: controller.galleryModalObj[index]);
+        },
+      ),
     );
   }
+}
 
-  Widget _buildChildWidget() {
-  return Text("data");
+
+
+class GridItemWidget extends StatelessWidget {
+  final GalleryModal item;
+
+  GridItemWidget({required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color:Colors.green,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              item.title.toString(),
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: item.featureImage != null
+                ? Image.network(item.featureImage!, fit: BoxFit.cover)
+                : Center(
+              child: Text(
+                item.title ?? '',
+                style: TextStyle(color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
-
-
-
-
-
 }
