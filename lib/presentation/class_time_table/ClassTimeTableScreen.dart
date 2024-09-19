ee07c7f958158f0c563877851c4fe6b7eb1124f4
criteriontech_flutter <flutterdeveloperteam@gmail.com>
2024-09-19 13:42:41 +0530

    day change

--- lib/presentation/class_time_table/ClassTimeTableScreen.dart
+++ lib/presentation/class_time_table/ClassTimeTableScreen.dart
@@ -2,6 +2,7 @@ import 'package:get_storage/get_storage.dart';
 import 'package:lerno/core/app_export.dart';
 import 'package:flutter/material.dart';
 import 'package:lerno/presentation/class_time_table/controller/class_time_table_controller.dart';
+import 'package:lerno/presentation/class_time_table/weekTab.dart';
 
 import '../common_widgets/CommonCard.dart';
 import '../common_widgets/MainBody.dart';
@@ -22,7 +23,11 @@ class _ClassTimeTableScreenState extends State<ClassTimeTableScreen> {
       label: 'Your Class\nTimetable!',
       imageUrl: 'assets/projectImages/assignmentpage.jpg',
       AppbarTitle: 'Class Timetable',
-      widget: _buildChildWidget(),
+      widget: Column(
+        children: [
+          _buildChildWidget(),
+        ],
+      ),
     );
   }
 
@@ -36,40 +41,19 @@ class _ClassTimeTableScreenState extends State<ClassTimeTableScreen> {
             if (snapshot.connectionState != ConnectionState.done) {
               return CustomLoader(); // CustomLoader();
             } else {
-              return SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _buildTimeTableCard(
-                        title: 'Monday',
-                        day: controller
-                            .classTimeTableModelObj.value!.timetable!.monday!),
-                    _buildTimeTableCard(
-                        title: 'Tuesday',
-                        day: controller
-                            .classTimeTableModelObj.value!.timetable!.tuesday!),
-                    _buildTimeTableCard(
-                        title: 'Wednesday',
-                        day: controller.classTimeTableModelObj.value!.timetable!
-                            .wednesday!),
-                    _buildTimeTableCard(
-                        title: 'Thursday',
-                        day: controller.classTimeTableModelObj.value!.timetable!
-                            .thursday!),
-                    _buildTimeTableCard(
-                        title: 'Friday',
-                        day: controller
-                            .classTimeTableModelObj.value!.timetable!.friday!),
-                    _buildTimeTableCard(
-                        title: 'Saturday',
-                        day: controller.classTimeTableModelObj.value!.timetable!
-                            .saturday!),
-                    _buildTimeTableCard(
-                        title: 'Sunday',
-                        day: controller
-                            .classTimeTableModelObj.value!.timetable!.sunday!),
+              return Expanded(
+                child: WeekTabScreen(
+                  tabTitles: ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'],
+                  tabContents: [
+                    _buildTimeTableCard(context: context,  day: controller.classTimeTableModelObj.value!.timetable!.monday!),
+                    _buildTimeTableCard(context: context, day: controller.classTimeTableModelObj.value!.timetable!.tuesday!),
+                    _buildTimeTableCard(context: context, day: controller.classTimeTableModelObj.value!.timetable!.wednesday!),
+                    _buildTimeTableCard(context: context, day: controller.classTimeTableModelObj.value!.timetable!.thursday!),
+                    _buildTimeTableCard(context: context, day: controller.classTimeTableModelObj.value!.timetable!.friday!),
+                    _buildTimeTableCard(context: context, day: controller.classTimeTableModelObj.value!.timetable!.saturday!),
+                    _buildTimeTableCard(context: context, day: controller.classTimeTableModelObj.value!.timetable!.sunday!),
                   ],
-                ),
+                )
               );
             }
           },
@@ -78,87 +62,133 @@ class _ClassTimeTableScreenState extends State<ClassTimeTableScreen> {
     );
   }
 
-  Widget _buildTimeTableCard({required String title, required List<Day> day}) {
-
-    return CommonCard(
-        title: title,
-        newWidget: day.length > 0
-            ?DataTable(
-              dividerThickness: 0.1,
-             columnSpacing: 40.0, // Adjust the spacing between columns as needed
-              columns:  <DataColumn>[
-                DataColumn(
-                  label: Text(
-                    'Time',
-                    style: theme.textTheme.titleMedium!.copyWith(color: Colors.lightBlue,fontSize: 16),
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Subject',
-                    style: theme.textTheme.titleMedium!.copyWith(color: Colors.lightBlue,fontSize: 16),
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Room',
-                    style:  theme.textTheme.titleMedium!.copyWith(color: Colors.lightBlue,fontSize: 16),
-                  ),
-                ),
-              ],
-              rows: day
-                  .map<DataRow>((Day singleDay) => DataRow(
-
-                cells: <DataCell>[
-
-                  DataCell(
-                    Row(
-                      children: [
-                        Text(
-                          singleDay.startTime!+'-',
-                          style: theme.textTheme.titleMedium!,
-                        ), Text(
-                           singleDay.endTime!,
-                          style: theme.textTheme.titleMedium!,
-                        ),
-                      ],
-                    ),
-                  ),
-                  DataCell(
-                    FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Text(
-                        '${singleDay.subjectName!} (${singleDay.sectionId!})',
-                        style: theme.textTheme.titleMedium,
-                      ),
-                    ),
-                  ),
-                  DataCell(
-                    FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Text(
-                        singleDay.roomNo!,
-                        style: theme.textTheme.titleMedium,
-                      ),
-                    ),
-                  ),
-                ],
-              ))
-                  .toList(),
-            )
-
-            : Center(
-                child: Column(
-                children: [
-                  Image.asset(
-                    "assets/projectImages/no_data.png",
-                    height: 80,
-                  ),
-                  Text(
-                    'No data found!',
-                    style: theme.textTheme.titleMedium,
-                  )
-                ],
-              )));
+  Widget _buildTimeTableCard({required BuildContext context,  required List<Day> day}) {
+    return SubjectWiseScheduleCard(
+      day: day,
+      theme: Theme.of(context),
+    );
   }
+}
+
+
+class SubjectWiseScheduleCard extends StatelessWidget {
+
+final List<Day> day;
+final ThemeData theme;
+
+const SubjectWiseScheduleCard({
+Key? key,
+required this.day,
+required this.theme,
+}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+  if (day.isEmpty) {
+    return _buildNoDataWidget();
+  }
+
+  // Group the schedule by subject
+  Map<String, List<Day>> subjectWiseSchedule = {};
+  for (var schedule in day) {
+    String subjectKey = '${schedule.subjectName} (${schedule.sectionId})';
+    if (!subjectWiseSchedule.containsKey(subjectKey)) {
+      subjectWiseSchedule[subjectKey] = [];
+    }
+    subjectWiseSchedule[subjectKey]!.add(schedule);
+  }
+
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Expanded(
+        child: ListView.builder(
+          itemCount: subjectWiseSchedule.length,
+          itemBuilder: (context, index) {
+            String subject = subjectWiseSchedule.keys.elementAt(index);
+            List<Day> schedules = subjectWiseSchedule[subject]!;
+            return _buildSubjectCard(subject, schedules);
+          },
+        ),
+      ),
+    ],
+  );
+}
+
+Widget _buildSubjectCard(String subject, List<Day> schedules) {
+  return Container(
+    margin: const  EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),// Space between cards
+    decoration: BoxDecoration(
+      color: Colors.white,
+      border: Border.all(color:  Colors.grey.shade300,), // Set your desired border color
+      borderRadius: BorderRadius.circular(10), // Rounded corners
+    ),
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            subject,
+            style: theme.textTheme.titleMedium!.copyWith(fontSize: 14,fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(height: 8.0),
+          ...schedules.map((schedule) => _buildScheduleRow(schedule)).toList(),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget _buildScheduleRow(Day schedule) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 4.0),
+    child: Row(
+      children: [
+        Expanded(
+          flex: 2,
+          child: Text(
+            '${schedule.startTime} - ${schedule.endTime}',
+            style: theme.textTheme.titleMedium!.copyWith(fontSize: 14,color: Colors.grey.shade700),
+          ),
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              "Room No.  ",
+              style: theme.textTheme.titleMedium!.copyWith(fontSize: 13,fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "${schedule.roomNo!}",
+              style: theme.textTheme.titleMedium!.copyWith(fontSize: 13,color: Colors.grey.shade700),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _buildNoDataWidget() {
+  return Card(
+    margin: const EdgeInsets.all(8.0),
+    child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/projectImages/no_data.png",
+            height: 80,
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'No data found!',
+            style: theme.textTheme.titleMedium,
+          )
+        ],
+      ),
+    ),
+  );
+}
 }
---
+++ lib/presentation/class_time_table/weekTab.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class WeekTabScreen extends StatelessWidget {
+  final List<String> tabTitles;
+  final List<Widget> tabContents;
+
+  const WeekTabScreen({
+    Key? key,
+    required this.tabTitles,
+    required this.tabContents,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: tabTitles.length,
+      child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                final tabWidth = constraints.maxWidth / tabTitles.length;
+                return TabBar(
+              dividerHeight: 0.0,
+                  tabs: tabTitles.map((title) => SizedBox(
+                    width: tabWidth,
+                    child: Tab(text: title),
+                  )).toList(),
+                  isScrollable: false,
+                  labelColor: Colors.white,
+                  unselectedLabelColor: Colors.black54,
+                  indicator: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  labelStyle: const TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+                  unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.normal, fontSize: 12),
+                  labelPadding: EdgeInsets.zero,
+                  indicatorPadding: EdgeInsets.zero,
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: TabBarView(
+              children: tabContents,
+              physics: const NeverScrollableScrollPhysics(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
